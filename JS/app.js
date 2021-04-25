0ab603974b9e8045eea081815d071741a210b554
Momansi96 <mansimohammed8@gmail.com>
2021-04-25 23:29:36 +0300

    finished class1

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let name = prompt('What is your name?');
+console.log ('HI ' +name); 
+
+let age = prompt('How old are you?');
+console.log ('you are ' +age + ' years old'); 
+
+let type = prompt('Do ypu prefer movies or TV shows?');
+console.log ('you prefer ' +type); 
+
+let way = prompt('Do you prefer to watch on netflix or regualer TV?');
+console.log ('you prefer to watch on ' +way); 
+
+alert('Hi ' + name + ' you are ' +age + ' years old, ' + 'you prefer to watch ' +type + ' on ' +way);
+
