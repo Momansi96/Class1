92be6b23a1007cc1ddfd86609d126359c325a262
Momansi96 <mansimohammed8@gmail.com>
2021-04-26 18:47:57 +0300

    finished class1

--- JS/app.js
+++ JS/app.js
@@ -2,15 +2,24 @@
 
 let name = prompt('What is your name?');
 console.log ('HI ' +name); 
+document.write('HI ' +name);
 
 let age = prompt('How old are you?');
 console.log ('you are ' +age + ' years old'); 
+document.write(' you are ' +age + ' years old ');
 
 let type = prompt('Do ypu prefer movies or TV shows?');
 console.log ('you prefer ' +type); 
+document.write('you prefer ' +type);
 
 let way = prompt('Do you prefer to watch on netflix or regualer TV?');
-console.log ('you prefer to watch on ' +way); 
+console.log ('you prefer to watch them on ' +way); 
+document.write(' you prefer to watch them on ' +way);
 
-alert('Hi ' + name + ' you are ' +age + ' years old, ' + 'you prefer to watch ' +type + ' on ' +way);
+
+let answer = ('Hi ' + name + ' you are ' +age + ' years old, ' + 'you prefer to watch ' +type + ' on ' +way);
+ 
+alert(answer); 
+
+
 
